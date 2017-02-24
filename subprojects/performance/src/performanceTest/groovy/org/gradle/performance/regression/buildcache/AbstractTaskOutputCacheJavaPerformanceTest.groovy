a0c539d8d175407f6f391b236fd0dea8352f403b
Jendrik Johannes <jendrik@gradle.com>
2017-02-24 08:05:49 +0100

    Increase memory for performance tests

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/buildcache/AbstractTaskOutputCacheJavaPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/buildcache/AbstractTaskOutputCacheJavaPerformanceTest.groovy
@@ -47,8 +47,8 @@ class AbstractTaskOutputCacheJavaPerformanceTest extends AbstractCrossVersionPer
      */
     def getScenarios() {
         [
-            ['largeMonolithicJavaProject', '2g', ['assemble']],
-            ['largeJavaMultiProject', '2g', ['assemble']]
+            ['largeMonolithicJavaProject', '4g', ['assemble']],
+            ['largeJavaMultiProject', '4g', ['assemble']]
         ]
     }
 
