4c4d37aefe749781f241a3c656bbd8104e19ed41
Jendrik Johannes <jendrik@gradle.com>
2017-02-24 15:00:37 +0100

    Remove outdated arguments

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/buildcache/HttpTaskOutputCacheJavaPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/buildcache/HttpTaskOutputCacheJavaPerformanceTest.groovy
@@ -62,7 +62,7 @@ class HttpTaskOutputCacheJavaPerformanceTest extends AbstractTaskOutputCacheJava
         })
     }
 
-    def "Builds '#testProject' calling #tasks with remote http cache"(String testProject, String heapSize, List<String> tasks) {
+    def "Builds '#testProject' calling #tasks with remote http cache"(String testProject, List<String> tasks) {
         runner.testId = "cached ${tasks.join(' ')} $testProject project - remote http cache"
         runner.testProject = testProject
         runner.tasksToRun = tasks
@@ -78,7 +78,7 @@ class HttpTaskOutputCacheJavaPerformanceTest extends AbstractTaskOutputCacheJava
         [testProject, tasks] << scenarios
     }
 
-    def "Builds '#testProject' calling #tasks with remote https cache"(String testProject, String heapSize, List<String> tasks) {
+    def "Builds '#testProject' calling #tasks with remote https cache"(String testProject, List<String> tasks) {
         runner.testId = "cached ${tasks.join(' ')} $testProject project - remote https cache"
         runner.testProject = testProject
         runner.tasksToRun = tasks
